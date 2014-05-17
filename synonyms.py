f52074ffc7dae518ebc24f2c391a8af65659a10d
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-05-17 10:29:53 +0300

    Refactor code

--- synonyms.py
+++ synonyms.py
@@ -7,5 +7,15 @@ def get_synonyms(word):
     return [a_synset for a_synset in synsets]
 
 if __name__ == '__main__':
-        for synset in get_synonyms(sys.argv[1]):
-            print 'As in %s: %s' % (synset.name.split('.')[0], wn.synset(synset.name).lemma_names)
+
+    if len(sys.argv) < 2:
+        word = raw_input('Please input a word to begin look-up: ')
+    else:
+        word = sys.argv[1]
+
+    print('\nSynonyms for "{}"'.format(word.capitalize()))
+
+    for synset in get_synonyms(word):
+        print('\nAs in {}:\n'.format(synset.name.split('.')[0]))
+        for lemma in wn.synset(synset.name).lemma_names:
+            print('  {}'.format(lemma))
