be211aafb2d502c401dbcd45d4a3ff006dcc1596
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-05-17 11:09:40 +0300

    Output format for using dict

--- synonyms.py
+++ synonyms.py
@@ -3,12 +3,15 @@ import sys
 
 
 def get_synonyms(word):
+    '''Returns a dict in the form of
+    {'semantic identifier': ['syn1', 'syn2', ...]}'''
     synsets = wn.synsets(word)
     return {syn.name.split('.')[0]: wn.synset(syn.name).lemma_names
             for syn in synsets}
 
 if __name__ == '__main__':
 
+    # If run with arg use the arg, otherwise prompt for word input.
     if len(sys.argv) < 2:
         word = raw_input('Please input a word to begin look-up: ')
     else:
@@ -16,7 +19,7 @@ if __name__ == '__main__':
 
     print('\nSynonyms for "{}"'.format(word.capitalize()))
 
-    for synset in get_synonyms(word):
-        print('\nAs in {}:\n'.format(synset.name.split('.')[0]))
-        for lemma in wn.synset(synset.name).lemma_names:
-            print('  {}'.format(lemma))
+    synonym_dict = get_synonyms(word)
+    for semantic_group in synonym_dict:
+        print('\nAs in {0}:\n'.format(semantic_group))
+        print(', '.join(synonym_dict[semantic_group]))
