7e3a82a50c26d7dc99520381501f457a5a1b88ff
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-05-31 16:04:41 +0300

    Add stub for tests

---
+++ run_tests.py
@@ -0,0 +1,22 @@
+import thesaurus as th
+import unittest
+
+class TestSynonyms(unittest.TestCase):
+
+    def test_synonyms(self):
+        pass
+
+class TestAntonyms(unittest.TestCase):
+
+    def test_antonyms(self):
+        pass
+
+
+class TestRelatedForms(unittest.TestCase):
+
+    def test_related_forms(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
